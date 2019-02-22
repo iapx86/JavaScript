bf771575a47b3e47fdc79c12bea6cd490037ac22
iapx86 <iapx86@gmail.com>
2019-02-22 10:13:25 +0900

    効果音再生の追加

--- ay-3-8910.js
+++ ay-3-8910.js
@@ -7,7 +7,7 @@
 const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 
 class AY_3_8910 {
-	constructor(clock, base) {
+	constructor(clock, base, se = [], freq = 11025) {
 		const sampleRate = 96000, repeat = 16;
 		this.toneBuffer = audioCtx.createBuffer(1, 2 * repeat, sampleRate);
 		for (let data = this.toneBuffer.getChannelData(0), i = 0; i < data.length; i++)
@@ -20,12 +20,13 @@ class AY_3_8910 {
 		}
 		this.rate = clock / 8 * repeat / sampleRate;
 		this.base = base;
+		this.se = se;
 		this.gainNode = audioCtx.createGain();
 		this.gainNode.connect(audioCtx.destination);
 		this.merger = audioCtx.createChannelMerger(1);
 		this.merger.connect(this.gainNode);
 		this.channel = [];
-		for (let i = 0; i < 6; i++) {
+		for (let i = 0; i < 6; i++)
 			this.channel[i] = new (function (dst, buf) {
 				this.freq = 0;
 				this.vol = 0;
@@ -39,6 +40,14 @@ class AY_3_8910 {
 				this.audioBufferSource.connect(this.gainNode);
 				this.audioBufferSource.start();
 			})(this.merger, i < 3 ? this.toneBuffer : this.noiseBuffer);
+		for (let i = 0; i < se.length; i++) {
+			const n = se[i].buf.length;
+			se[i].audioBuffer = audioCtx.createBuffer(1, n, 44100);
+			se[i].playbackRate = freq / 44100;
+			const data = se[i].audioBuffer.getChannelData(0);
+			for (let j = 0; j < n; j++)
+				data[j] = se[i].buf[j] / 32767;
+			se[i].audioBufferSource = null;
 		}
 	}
 
@@ -69,6 +78,23 @@ class AY_3_8910 {
 				ch.gainNode.gain.value = vol === 0 ? 0 : Math.pow(2, (vol - 15) / 2) / 10;
 				ch.vol = vol;
 			}
+		}
+		for (let i = 0, n = this.se.length; i < n; i++) {
+			const se = this.se[i];
+			if (se.stop && se.audioBufferSource) {
+				se.audioBufferSource.stop();
+				se.audioBufferSource = null;
+			}
+			if (se.start && !se.audioBufferSource) {
+				se.audioBufferSource = audioCtx.createBufferSource();
+				se.audioBufferSource.buffer = se.audioBuffer;
+				se.audioBufferSource.loop = se.loop;
+				se.audioBufferSource.playbackRate.value = se.playbackRate;
+				se.audioBufferSource.connect(this.merger);
+				se.audioBufferSource.start();
+			}
+			se.start = false;
+			se.stop = false;
 		}
 	}
 }
