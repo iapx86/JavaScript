65a38e1e2855a5d7caad14a3cfa6ccda2290a110
iapx86 <iapx86@gmail.com>
2019-03-02 12:59:32 +0900

    Firefox, Edge対策

--- pac-man_sound.js
+++ pac-man_sound.js
@@ -23,18 +23,15 @@ class PacManSound {
 		this.merger = audioCtx.createChannelMerger(1);
 		this.merger.connect(this.gainNode);
 		this.channel = [];
-		for (let i = 0; i < 3; i++) {
+		for (let i = 0; i < 3; i++)
 			this.channel[i] = new (function (dst, buf) {
 				this.voice = 0;
 				this.freq = 0;
 				this.vol = 0;
 				this.gainNode = [];
 				this.audioBufferSource = [];
-				this.gainNode2 = audioCtx.createGain();
-				this.gainNode2.gain.value = 0;
-				this.gainNode2.connect(dst);
 				this.merger = audioCtx.createChannelMerger(1);
-				this.merger.connect(this.gainNode2);
+				this.merger.connect(dst);
 				for (let i = 0; i < 8; i++) {
 					this.gainNode[i] = audioCtx.createGain();
 					this.gainNode[i].gain.value = 0;
@@ -47,7 +44,6 @@ class PacManSound {
 					this.audioBufferSource[i].start();
 				}
 			})(this.merger, this.audioBuffer);
-		}
 		for (let i = 0; i < se.length; i++) {
 			const n = se[i].buf.length;
 			se[i].audioBuffer = audioCtx.createBuffer(1, n, 48000);
@@ -62,85 +58,59 @@ class PacManSound {
 	output() {
 		const now = audioCtx.currentTime;
 		const nextTime = now + 1 / 120;
-		const base = this.base;
 		let voice, freq, vol;
 
 		for (let i = 0; i < 3; i++) {
-			switch (i) {
-			case 0:
-				voice = base[0x05] & 7;
-				freq = base[0x10] | base[0x11] << 4 | base[0x12] << 8 | base[0x13] << 12 | base[0x14] << 16;
-				vol = base[0x15];
-				break;
-			case 1:
-				voice = base[0x0a] & 7;
-				freq = base[0x16] << 4 | base[0x17] << 8 | base[0x18] << 12 | base[0x19] << 16;
-				vol = base[0x1a];
-				break;
-			case 2:
-				voice = base[0x0f] & 7;
-				freq = base[0x1b] << 4 | base[0x1c] << 8 | base[0x1d] << 12 | base[0x1e] << 16;
-				vol = base[0x1f];
-				break;
-			}
+			[voice, freq, vol] = this.getParameter(this.base, i);
 			const ch = this.channel[i];
 			if (voice !== ch.voice) {
-				ch.gainNode[ch.voice].gain.value = 0;
-				ch.audioBufferSource[voice].playbackRate.value = freq / (1 << 14);
-				ch.gainNode[voice].gain.value = freq !== 0;
+				ch.gainNode[ch.voice].gain.cancelScheduledValues(now);
+				ch.gainNode[ch.voice].gain.setValueAtTime(0, now);
+				ch.audioBufferSource[voice].playbackRate.cancelScheduledValues(now);
+				ch.audioBufferSource[voice].playbackRate.setValueAtTime(freq / (1 << 14), now);
+				ch.gainNode[voice].gain.cancelScheduledValues(now);
+				ch.gainNode[voice].gain.setValueAtTime(freq > 0 ? vol / (15 * 10) : 0, now);
 				ch.voice = voice;
 				ch.freq = freq;
+				ch.vol = vol;
 			}
 			else if (freq !== ch.freq) {
-				ch.audioBufferSource[voice].playbackRate.value = freq / (1 << 14);
-				ch.gainNode[voice].gain.value = freq !== 0;
+				ch.audioBufferSource[voice].playbackRate.cancelScheduledValues(now);
+				ch.audioBufferSource[voice].playbackRate.setValueAtTime(freq / (1 << 14), now);
+				ch.gainNode[voice].gain.cancelScheduledValues(now);
+				ch.gainNode[voice].gain.setValueAtTime(freq > 0 ? vol / (15 * 10) : 0, now);
 				ch.freq = freq;
+				ch.vol = vol;
 			}
-			if (vol !== ch.vol) {
-				ch.gainNode2.gain.value = vol / (15 * 10);
+			else if (vol !== ch.vol) {
+				ch.gainNode[voice].gain.cancelScheduledValues(now);
+				ch.gainNode[voice].gain.setValueAtTime(freq > 0 ? vol / (15 * 10) : 0, now);
 				ch.vol = vol;
 			}
 		}
-		if (this.base2 !== null) {
-			const base = this.base2;
-
+		if (this.base2 !== null)
 			for (let i = 0; i < 3; i++) {
-				switch (i) {
-					case 0:
-						voice = base[0x05] & 7;
-						freq = base[0x10] | base[0x11] << 4 | base[0x12] << 8 | base[0x13] << 12 | base[0x14] << 16;
-						vol = base[0x15];
-						break;
-					case 1:
-						voice = base[0x0a] & 7;
-						freq = base[0x16] << 4 | base[0x17] << 8 | base[0x18] << 12 | base[0x19] << 16;
-						vol = base[0x1a];
-						break;
-					case 2:
-						voice = base[0x0f] & 7;
-						freq = base[0x1b] << 4 | base[0x1c] << 8 | base[0x1d] << 12 | base[0x1e] << 16;
-						vol = base[0x1f];
-						break;
-				}
+				[voice, freq, vol] = this.getParameter(this.base2, i);
 				const ch = this.channel[i];
 				if (voice !== ch.voice) {
 					ch.gainNode[ch.voice].gain.setValueAtTime(0, nextTime);
 					ch.audioBufferSource[voice].playbackRate.setValueAtTime(freq / (1 << 14), nextTime);
-					ch.gainNode[voice].gain.setValueAtTime(freq !== 0, nextTime);
+					ch.gainNode[voice].gain.setValueAtTime(freq > 0 ? vol / (15 * 10) : 0, nextTime);
 					ch.voice = voice;
 					ch.freq = freq;
+					ch.vol = vol;
 				}
 				else if (freq !== ch.freq) {
 					ch.audioBufferSource[voice].playbackRate.setValueAtTime(freq / (1 << 14), nextTime);
-					ch.gainNode[voice].gain.setValueAtTime(freq !== 0, nextTime);
+					ch.gainNode[voice].gain.setValueAtTime(freq > 0 ? vol / (15 * 10) : 0, nextTime);
 					ch.freq = freq;
+					ch.vol = vol;
 				}
-				if (vol !== ch.vol) {
-					ch.gainNode2.gain.setValueAtTime(vol / (15 * 10), nextTime);
+				else if (vol !== ch.vol) {
+					ch.gainNode[voice].gain.setValueAtTime(freq > 0 ? vol / (15 * 10) : 0, nextTime);
 					ch.vol = vol;
 				}
 			}
-		}
 		for (let i = 0, n = this.se.length; i < n; i++) {
 			const se = this.se[i];
 			if (se.stop && se.audioBufferSource) {
@@ -158,6 +128,29 @@ class PacManSound {
 			se.start = false;
 			se.stop = false;
 		}
+	}
+
+	getParameter(base, index) {
+		let voice, freq, vol;
+
+		switch (index) {
+		case 0:
+			voice = base[0x05] & 7;
+			freq = base[0x10] | base[0x11] << 4 | base[0x12] << 8 | base[0x13] << 12 | base[0x14] << 16;
+			vol = base[0x15];
+			break;
+		case 1:
+			voice = base[0x0a] & 7;
+			freq = base[0x16] << 4 | base[0x17] << 8 | base[0x18] << 12 | base[0x19] << 16;
+			vol = base[0x1a];
+			break;
+		case 2:
+			voice = base[0x0f] & 7;
+			freq = base[0x1b] << 4 | base[0x1c] << 8 | base[0x1d] << 12 | base[0x1e] << 16;
+			vol = base[0x1f];
+			break;
+		}
+		return [voice, freq, vol];
 	}
 }
 
