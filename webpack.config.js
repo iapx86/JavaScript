cf35903e6781852677c4dd8bebf486c2ec6f6c1f
iapx86 <iapx86@gmail.com>
2020-08-04 01:15:01 +0900

    ワールドコートを追加

--- webpack.config.js
+++ webpack.config.js
@@ -66,6 +66,7 @@ const list = [
 	{name: 'vulgus', title: 'Vulgus', width: 224, height: 256},
 	{name: 'warp_and_warp', title: 'Warp & Warp', width: 224, height: 272},
 	{name: 'wonder_momo', title: 'Wonder Momo', width: 288, height: 224},
+	{name: 'world_court', title: 'World Court', width: 288, height: 224},
 	{name: 'xevious', title: 'Xevious', width: 224, height: 288},
 	{name: 'zigzag', title: 'Zig Zag', width: 224, height: 256},
 ];
