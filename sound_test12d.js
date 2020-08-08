b9a327c8307d7571c870a603652c2e108365d41f
iapx86 <iapx86@gmail.com>
2020-08-08 23:33:10 +0900

    PCMを追加

--- sound_test12d.js
+++ sound_test12d.js
@@ -6,8 +6,10 @@
 
 import YM2151 from './ym2151.js';
 import C30 from './c30.js';
+import Dac8Bit2Ch from './dac_8bit_2ch.js';
 import {dummypage, init, loop, canvas} from './main.js';
 import MC6809 from './mc6809.js';
+import MC6801 from './mc6801.js';
 let game, sound;
 
 class SoundTest {
@@ -25,10 +27,13 @@ class SoundTest {
 		// CPU周りの初期化
 		this.ram2 = new Uint8Array(0x800).addBase();
 		this.ram3 = new Uint8Array(0x2000).addBase();
+		this.ram4 = new Uint8Array(0x900).addBase();
 		this.fm = {addr: 0, reg: new Uint8Array(0x100), kon: new Uint8Array(8), status: 0, timera: 0, timerb: 0};
 		this.count = 0;
 		this.bank3 = 0x40;
+		this.bank4 = 0x80;
 		this.cpu3_irq = false;
+		this.mcu_irq = false;
 
 		this.cpu3 = new MC6809(this);
 		for (let i = 0; i < 0x40; i++)
@@ -72,6 +77,49 @@ class SoundTest {
 		this.cpu3.memorymap[0xe0].write = () => void(this.cpu3_irq = false);
 
 		this.cpu3.check_interrupt = () => this.cpu3_irq && this.cpu3.interrupt() || (this.fm.status & 3) !== 0 && this.cpu3.fast_interrupt();
+
+		this.mcu = new MC6801(this);
+		this.mcu.memorymap[0].base = this.ram4.base[0];
+		this.mcu.memorymap[0].read = addr => {
+			switch (addr) {
+			case 2:
+				return 0xf8;
+			case 8:
+				const data = this.ram4[8];
+				this.ram4[8] &= ~0xe0;
+				return data;
+			}
+			return this.ram4[addr];
+		}
+		this.mcu.memorymap[0].write = (addr, data) => {
+			if (addr === 3) {
+				sound[2].channel[0].gain = ((data >> 1 & 2 | data & 1) + 1) / 4;
+				sound[2].channel[1].gain = ((data >> 3 & 3) + 1) / 4;
+			}
+			this.ram4[addr] = data;
+		}
+		for (let i = 0; i < 0x80; i++)
+			this.mcu.memorymap[0x40 + i].base = VOI.base[0x80 + i];
+		for (let i = 0; i < 8; i++) {
+			this.mcu.memorymap[0xc0 + i].base = this.ram2.base[i];
+			this.mcu.memorymap[0xc0 + i].write = null;
+		}
+		this.mcu.memorymap[0xc0].write = (addr, data) => void(addr === 0xc000 && this.ram2[0] === 0xa6 || (this.ram2[addr & 0xff] = data));
+		for (let i = 0; i < 8; i++) {
+			this.mcu.memorymap[0xc8 + i].base = this.ram4.base[1 + i];
+			this.mcu.memorymap[0xc8 + i].write = null;
+		}
+		this.mcu.memorymap[0xd0].write = (addr, data) => sound[2].write(0, data, this.count);
+		this.mcu.memorymap[0xd4].write = (addr, data) => sound[2].write(1, data, this.count);
+		this.mcu.memorymap[0xd8].write = (addr, data) => {
+			const index = [0xf8, 0xf4, 0xec, 0xdc, 0xbc, 0x7c].indexOf(data & 0xfc);
+			this.bankswitch4(index > 0 ? index << 9 | data << 7 & 0x180 : index === 0 ? data << 7 & 0x180 ^ 0x100 : 0);
+		};
+		for (let i = 0; i < 0x10; i++)
+			this.mcu.memorymap[0xf0 + i].base = MCU.base[i];
+		this.mcu.memorymap[0xf0].write = () => void(this.mcu_irq = false);
+
+		this.mcu.check_interrupt = () => this.mcu_irq && this.mcu.interrupt() || (this.ram4[8] & 0x40) !== 0 && this.mcu.interrupt('ocf');
 	}
 
 	bankswitch3(bank) {
@@ -86,9 +134,21 @@ class SoundTest {
 		this.bank3 = bank;
 	}
 
+	bankswitch4(bank) {
+		if (bank === this.bank4)
+			return;
+		if (bank < 0xa00)
+			for (let i = 0; i < 0x80; i++)
+				this.mcu.memorymap[0x40 + i].base = VOI.base[bank + i];
+		else
+			for (let i = 0; i < 0x80; i++)
+				this.mcu.memorymap[0x40 + i].base = dummypage;
+		this.bank4 = bank;
+	}
+
 	execute() {
-		this.cpu3_irq = true;
-		for (this.count = 0; this.count < 58; this.count++) { // 3579580 / 60 / 1024
+		this.cpu3_irq = this.mcu_irq = true;
+		for (this.count = 0; this.count < 29; this.count++) {
 			this.cpu3.execute(146);
 			if ((this.fm.reg[0x14] & 1) !== 0 && (this.fm.timera += 16) >= 0x400) {
 				this.fm.timera = (this.fm.timera & 0x3ff) + (this.fm.reg[0x10] << 2 | this.fm.reg[0x11] & 3);
@@ -98,6 +158,25 @@ class SoundTest {
 				this.fm.timerb = (this.fm.timerb & 0xff) + this.fm.reg[0x12];
 				this.fm.status |= this.fm.reg[0x14] >> 2 & 2;
 			}
+		}
+		for (this.count = 0; this.count < 50; this.count++) {
+			(this.ram4[8] & 8) !== 0 && (this.ram4[8] |= 0x40);
+			this.mcu.execute(84);
+		}
+		for (this.count = 29; this.count < 58; this.count++) { // 3579580 / 60 / 1024
+			this.cpu3.execute(146);
+			if ((this.fm.reg[0x14] & 1) !== 0 && (this.fm.timera += 16) >= 0x400) {
+				this.fm.timera = (this.fm.timera & 0x3ff) + (this.fm.reg[0x10] << 2 | this.fm.reg[0x11] & 3);
+				this.fm.status |= this.fm.reg[0x14] >> 2 & 1;
+			}
+			if ((this.fm.reg[0x14] & 2) !== 0 && ++this.fm.timerb >= 0x100) {
+				this.fm.timerb = (this.fm.timerb & 0xff) + this.fm.reg[0x12];
+				this.fm.status |= this.fm.reg[0x14] >> 2 & 2;
+			}
+		}
+		for (this.count = 50; this.count < 100; this.count++) {
+			(this.ram4[8] & 8) !== 0 && (this.ram4[8] |= 0x40);
+			this.mcu.execute(84);
 		}
 		return this;
 	}
@@ -112,6 +191,8 @@ class SoundTest {
 			if (this.command[0] < 0) {
 				for (let i = 0; i < 0x80; i++)
 					sound[1].write(0x240 + i, 0);
+				this.ram2[0x2b] = 0;
+				this.ram2[0x2c] = 0x40;
 				this.ram2[0x101] = 0;
 				this.ram2.set([0, 1, 0, 0, 0, 0, 0, 0], 0x30);
 				this.ram2[0x2f] = 8;
@@ -133,9 +214,11 @@ class SoundTest {
 			this.fReset = false;
 			this.nSound = 0;
 			this.command.splice(0);
-			this.cpu3_irq = false;
+			this.cpu3_irq = this.mcu_irq = false;
 			this.bankswitch3(0x40);
+			this.bankswitch4(0x80);
 			this.cpu3.reset();
+			this.mcu.reset();
 			sound[1].write(0, 0xa6);
 		}
 		return this;
@@ -243,7 +326,7 @@ class SoundTest {
 
 const key = [];
 const url = 'dspirit.zip';
-let SND;
+let SND, MCU, VOI;
 
 window.addEventListener('load', () => {
 	const tmp = Object.assign(document.createElement('canvas'), {width: 28, height: 16});
@@ -257,11 +340,15 @@ window.addEventListener('load', () => {
 
 function success(zip) {
 	SND = new Uint8Array((zip.files['ds1_s0.bin'].inflate() + zip.files['ds1_s1.bin'].inflate()).split('').map(c => c.charCodeAt(0))).addBase();
+	MCU = new Uint8Array(zip.files['cus64-64a1.mcu'].inflate().split('').map(c => c.charCodeAt(0))).addBase();
+	VOI = zip.files['ds1_v0.bin'].inflate() + zip.files['ds1_v0.bin'].inflate() + zip.files['ds_voi-1.bin'].inflate() + zip.files['ds_voi-2.bin'].inflate();
+	VOI = new Uint8Array((VOI + zip.files['ds_voi-3.bin'].inflate() + zip.files['ds_voi-4.bin'].inflate()).split('').map(c => c.charCodeAt(0))).addBase();
 	init({
 		game: game = new SoundTest(),
 		sound: sound = [
 			new YM2151({clock: 3579580, resolution: 58, gain: 2}),
 			new C30({clock: 49152000 / 2048 / 2, resolution: 58}),
+			new Dac8Bit2Ch({resolution: 100}),
 		],
 	});
 	game.initial = true;
