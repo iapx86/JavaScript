e868b9211efdc7fe9c2bee9f68387fead86622f5
iapx86 <iapx86@gmail.com>
2020-09-06 04:09:37 +0900

    サウンドテスト10dを追加

---
+++ sound_test10d.js
@@ -0,0 +1,218 @@
+/*
+ *
+ *	Sound Test 10d
+ *
+ */
+
+import YM2151 from './ym2151.js';
+import {init, loop, canvas} from './main.js';
+import Z80 from './z80.js';
+let game, sound;
+
+class SoundTest {
+	cxScreen = 224;
+	cyScreen = 256;
+	width = 256;
+	height = 256;
+	xOffset = 0;
+	yOffset = 0;
+
+	fReset = true;
+	nSound = 1;
+
+	ram2 = new Uint8Array(0x10000).addBase();
+	fm = {addr: 0, reg: new Uint8Array(0x100), kon: new Uint8Array(8), status: 0, timera: 0, timerb: 0};
+	count = 0;
+	command = [];
+	cpu2 = new Z80();
+
+	constructor() {
+		// CPU周りの初期化
+		for (let i = 0; i < 0x100; i++) {
+			this.cpu2.memorymap[i].base = this.ram2.base[i];
+			this.cpu2.memorymap[i].write = null;
+		}
+		for (let i = 0; i < 0x100; i++) {
+			this.cpu2.iomap[i].read = addr => {
+				switch (addr & 0xff) {
+				case 1:
+					return this.fm.status;
+				case 2:
+					return this.command.length ? this.command.shift() : 0xff;
+				}
+				return 0xff;
+			};
+			this.cpu2.iomap[i].write = (addr, data) => {
+				switch (addr & 0xff) {
+				case 0:
+					return void(this.fm.addr = data);
+				case 1:
+					switch (this.fm.addr) {
+					case 8: // KON
+						this.fm.kon[data & 7] = Number((data & 0x78) !== 0);
+						break;
+					case 0x14: // CSM/F RESET/IRQEN/LOAD
+						this.fm.status &= ~(data >> 4 & 3);
+						if ((data & ~this.fm.reg[0x14] & 1) !== 0)
+							this.fm.timera = this.fm.reg[0x10] << 2 | this.fm.reg[0x11] & 3;
+						if ((data & ~this.fm.reg[0x14] & 2) !== 0)
+							this.fm.timerb = this.fm.reg[0x12];
+						break;
+					}
+					return sound.write(this.fm.addr, this.fm.reg[this.fm.addr] = data, this.count);
+				}
+			};
+		}
+
+		this.cpu2.check_interrupt = () => (this.fm.status & 3) !== 0 && this.cpu2.interrupt(0xef);
+	}
+
+	execute() {
+		if (this.command.length)
+			this.cpu2.interrupt(0xdf);
+		for (this.count = 0; this.count < 58; this.count++) { // 3579545 / 60 / 1024
+			this.cpu2.execute(146);
+			if ((this.fm.reg[0x14] & 1) !== 0 && (this.fm.timera += 16) >= 0x400) {
+				this.fm.timera = (this.fm.timera & 0x3ff) + (this.fm.reg[0x10] << 2 | this.fm.reg[0x11] & 3);
+				this.fm.status |= this.fm.reg[0x14] >> 2 & 1;
+			}
+			if ((this.fm.reg[0x14] & 2) !== 0 && ++this.fm.timerb >= 0x100) {
+				this.fm.timerb = (this.fm.timerb & 0xff) + this.fm.reg[0x12];
+				this.fm.status |= this.fm.reg[0x14] >> 2 & 2;
+			}
+		}
+		return this;
+	}
+
+	reset() {
+		this.fReset = true;
+	}
+
+	updateStatus() {
+		// リセット処理
+		if (this.fReset) {
+			this.fReset = false;
+			this.nSound = 0;
+			this.ram2.fill(0);
+			this.ram2.set(PRG.subarray(0x400, 0xc400));
+			this.command.splice(0);
+			this.cpu2.reset();
+		}
+		return this;
+	}
+
+	updateInput() {
+		return this;
+	}
+
+	coin() {
+		return this;
+	}
+
+	start1P() {
+		return this;
+	}
+
+	start2P() {
+		return this;
+	}
+
+	up() {
+		return this;
+	}
+
+	right(fDown = false) {
+		if (fDown)
+			return this;
+		if (++this.nSound >= 0x100)
+			this.nSound = 0;
+		return this;
+	}
+
+	down() {
+		return this;
+	}
+
+	left(fDown = false) {
+		if (fDown)
+			return this;
+		if (--this.nSound < 0)
+			this.nSound = 0xff;
+		return this;
+	}
+
+	triggerA(fDown = false) {
+		if (fDown)
+			return this;
+		console.log(`command=$${this.nSound.toString(16)}`);
+		this.command.push(0x20, this.nSound);
+		return this;
+	}
+
+	triggerB(fDown = false) {
+		if (fDown)
+			return this;
+		this.command.push(0x20);
+		return this;
+	}
+
+	makeBitmap(data) {
+		for (let i = 0; i < 16; i++)
+			for (let j = 0; j < 8; j++)
+				SoundTest.Xfer28x16(data, 28 * j + 256 * 16 * i, key[i < 8 ? 0 : 13]);
+
+		for (let i = 0; i < 8; i++) {
+			const kc = this.fm.reg[0x28 + i], pitch = (kc >> 4 & 7) * 12 + (kc >> 2 & 3) * 3 + (kc & 3);
+			if (!this.fm.kon[i] || pitch < 0 || pitch >= 12 * 8)
+				continue;
+			SoundTest.Xfer28x16(data, 28 * Math.floor(pitch / 12) + 256 * 16 * i, key[pitch % 12 + 1]);
+		}
+	}
+
+	static Xfer28x16(data, dst, src) {
+		for (let i = 0; i < 16; i++)
+			for (let j = 0; j < 28; j++)
+				data[dst + 256 * i + j] = src[28 * i + j];
+	}
+}
+
+/*
+ *
+ *	Sound Test 10d
+ *
+ */
+
+const key = [];
+const url = 'bchopper.zip';
+const PRG = new Uint8Array(0x20000);
+
+window.addEventListener('load', () => {
+	const tmp = Object.assign(document.createElement('canvas'), {width: 28, height: 16});
+	const img = document.getElementsByTagName('img');
+	for (let i = 0; i < 14; i++) {
+		tmp.getContext('2d').drawImage(img['key' + i], 0, 0);
+		key.push(new Uint32Array(tmp.getContext('2d').getImageData(0, 0, 28, 16).data.buffer));
+	}
+	$.ajax({url, success, error: () => alert(url + ': failed to get')});
+});
+
+function success(zip) {
+	zip.files['mrheli/mh_c-h1-.ic41'].inflate().split('').forEach((c, i) => PRG[1 | i << 1] = c.charCodeAt(0));
+	zip.files['mrheli/mh_c-l1-.ic36'].inflate().split('').forEach((c, i) => PRG[i << 1] = c.charCodeAt(0));
+	init({
+		game: game = new SoundTest(),
+		sound: sound = new YM2151({clock: 3579545, resolution: 58}),
+	});
+	game.initial = true;
+	canvas.addEventListener('click', e => {
+		if (game.initial)
+			game.initial = false;
+		else if (e.offsetX < canvas.width / 2)
+			game.left();
+		else
+			game.right();
+		game.triggerA();
+	});
+	loop();
+}
+
