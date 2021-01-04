06066c7dec7b38126b1b23f915e18682bffc48e4
iapx86 <iapx86@gmail.com>
2021-01-04 17:54:55 +0900

    ワンダーボーイ モンスターランドを追加

--- webpack.config.js
+++ webpack.config.js
@@ -75,6 +75,7 @@ const list = [
 	{name: 'vulgus', title: 'Vulgus', width: 224, height: 256},
 	{name: 'warp_and_warp', title: 'Warp & Warp', width: 224, height: 272},
 	{name: 'wonder_boy', title: 'Wonder Boy', width: 256, height: 224},
+	{name: 'wonder_boy_in_monster_land', title: 'Wonder Boy in Monster Land', width: 256, height: 224},
 	{name: 'wonder_momo', title: 'Wonder Momo', width: 288, height: 224},
 	{name: 'world_court', title: 'World Court', width: 288, height: 224},
 	{name: 'xevious', title: 'Xevious', width: 224, height: 288},
